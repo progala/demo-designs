1bef7def6e2d0c7414dbca3c1a9292fb57d65682
Stephen Kiely <stephenkiely87@gmail.com>
2024-06-19 21:43:22 -0500

    Implement Controller Design

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,10 +1,12 @@
 """The __init__.py module is required for Nautobot to load the jobs via Git."""
 
 from .core_site import CoreSiteDesign
+from .device_controllers import ControllerDesign
 from .edge_site import EdgeDesign
 from .initial_data import InitialDesign
 
 __all__ = [
+    "ControllerDesign",
     "CoreSiteDesign",
     "EdgeDesign",
     "InitialDesign",
