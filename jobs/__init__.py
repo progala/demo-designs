5c515822f1489744960c84f633856a946e307eec
Stephen Kiely <stephenkiely87@gmail.com>
2024-10-21 09:13:59 -0500

    Alphabetize imports.

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,10 +1,9 @@
 """The __init__.py module is required for Nautobot to load the jobs via Git."""
 
-from .device_controllers import ControllerDesign
-from .device_modules import ModuleDesign
 from .cloud_networks import CloudNetworkDesign
 from .core_site import CoreSiteDesign
 from .device_controllers import ControllerDesign
+from .device_modules import ModuleDesign
 from .edge_site import EdgeDesign
 from .initial_data import InitialDesign
 
