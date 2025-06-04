440bc6804b008d4f974751cec9f6cbc0550cc4d2
Przemek Rogala <progala@progala.net>
2025-06-04 13:35:36 +0100

    Test

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -11,3 +11,51 @@ __all__ = [
     "InitialDesign",
     "P2PDesign",
 ]
+
+def network_stringz(network: str) -> str:
+    """Jinja2 filter to convert the IPNetwork object to a string.
+
+    If an attribute is supplied, first lookup the attribute on the IPNetwork
+    object, then convert the returned value to a string.
+
+    Args:
+        network (IPNetwork): Object to convert to string
+        attr (str, optional): Optional attribute to retrieve from the IPNetwork prior
+            to converting to a string. Defaults to "".
+
+    Example:
+    ```jinja
+        {{ "1.2.3.4/24" | ip_network | network_string("ip") }}
+    ```
+
+    Returns:
+        str: Converted object
+    """
+    return str(network)
+
+def random_stuff(stuff: str) -> str:
+    """Jinja2 filter to convert the IPNetwork object to a string.
+
+    If an attribute is supplied, first lookup the attribute on the IPNetwork
+    object, then convert the returned value to a string.
+
+    Args:
+        network (IPNetwork): Object to convert to string
+        attr (str, optional): Optional attribute to retrieve from the IPNetwork prior
+            to converting to a string. Defaults to "".
+
+    Example:
+    ```jinja
+        {{ "1.2.3.4/24" | ip_network | network_string("ip") }}
+    ```
+
+    Returns:
+        str: Converted object
+    """
+    return f"{stuff*5}"
+
+from django.template import engines
+
+jinja_env = engines['jinja'].env
+jinja_env.filters['network_stringz'] = network_stringz
+jinja_env.filters['random_stuff'] = random_stuff
