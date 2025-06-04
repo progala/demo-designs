62e084779db46061d8f66903e3bbe0a01ab7bb5a
Przemek Rogala <progala@progala.net>
2025-06-04 11:40:11 +0100

    Test uuid

--- jobs/edge_site/context/__init__.py
+++ jobs/edge_site/context/__init__.py
@@ -1,7 +1,18 @@
 """This module contains the render context for the basic design."""
+import os
+import uuid
 from nautobot_design_builder.context import Context, context_file
 
+NAMESPACE_UUID = os.getenv("NAMESPACE_UUID", "12345678-1234-5678-1234-567812345678")
 
 @context_file("context.yaml")
 class EdgeDesignContext(Context):
     """Render context for basic design."""
+
+    def deterministic_uuid(self, name: str, model: str) -> str:
+        namespace = uuid.UUID(NAMESPACE_UUID)
+        name = str(name).lower()
+        model = str(model).lower()
+        seed = f"{model}__{name}"
+        # Generate and return a UUID using the uuid5 method, which combines the namespace and the name
+        return str(uuid.uuid5(namespace, seed))
