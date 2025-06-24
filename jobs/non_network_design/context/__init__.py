9c8a657ace0e1968638565a9a8c8ebd250cc4d9c
Taylor <114100001+taylordawson4@users.noreply.github.com>
2025-06-24 16:54:32 -0500

    Update __init__.py

--- jobs/non_network_design/context/__init__.py
+++ jobs/non_network_design/context/__init__.py
@@ -1,1 +1,6 @@
+"""This module contains the render context for the non-network infra design."""
+from nautobot_design_builder.context import Context, context_file
 
+@context_file("context.yaml")
+class NonNetworkDesignContext(Context):
+    """Render context for non-network infra design."""
