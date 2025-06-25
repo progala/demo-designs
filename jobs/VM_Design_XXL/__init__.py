605d28fcfe9fe2e95fa99a52627f5c3ddf29b40b
Taylor <114100001+taylordawson4@users.noreply.github.com>
2025-06-25 15:23:38 -0500

    push

---
+++ jobs/VM_Design_XXL/__init__.py
@@ -0,0 +1,50 @@
+"""Non-networking demo design demonstrates the capabilities of the Design Builder."""
+
+from nautobot.apps.jobs import register_jobs, StringVar, ObjectVar, IntegerVar
+from nautobot.dcim.models import Location
+from nautobot.tenancy.models import Tenant
+from nautobot_design_builder.design_job import DesignJob
+from .context import NonNetworkDesignContext
+
+class NonNetworkDesign(DesignJob):
+    """A basic non-networking infra design for design builder."""
+    region = ObjectVar(
+        label="Region",
+        description="Select the region for this site",
+        model=Location,
+    )
+    site_name = StringVar(
+        label="Site Name",
+        regex=r"\w{3}\d+",
+        description="Unique name for the site."
+    )
+    cluster_name = StringVar(
+        label="Cluster Name",
+        default="Cluster-01"
+    )
+    tenant = ObjectVar(
+        label="Tenant",
+        model=Tenant,
+        required=False,
+        description="Optional tenant to assign resources to."
+    )
+    rack_name = StringVar(
+        label="Rack Name",
+        default="Rack-01"
+    )
+    vm_quantity = IntegerVar(
+        label="Number of VMs",
+        default=3
+    )
+    has_sensitive_variables = False
+
+    class Meta:
+        """Metadata describing this non-networking infra design job."""
+        name = "Non-Network Infra Design"
+        commit_default = False
+        design_file = "designs/0001_design.yaml.j2"
+        context_class = NonNetworkDesignContext
+        nautobot_version = ">=2"
+
+name = "Demo Designs"
+register_jobs(NonNetworkDesign)
